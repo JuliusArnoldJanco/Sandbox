d24a7d63194f9d4f5ad4b708e2fdab50ca7927d8
Juliusjanco <julius.arnoldjanco@my.jcu.edu.au>
2017-03-17 08:13:19 +1000

    Adding input name functionality (pulls every 2nd letter and checks for blanks

--- oddName.py
+++ oddName.py
@@ -1,1 +1,28 @@
 """"Julius Arnold-Janco"""
+
+userinput = input(">>")
+try:
+    if ' ' in userinput:
+        print('Please Enter Name:')
+
+    elif '  ' in userinput:
+        print("Only one space please")
+
+    elif userinput is '':
+        print("Something has to be put in porkchop")
+
+    else:
+        count = 2
+        for each in list(userinput):
+            if count % 2 != 0:
+                print(each)
+                count+=1
+            else:
+                count+=1
+except SyntaxError:
+    userinput = None
+    print("must enter something")
+
+
+
+
