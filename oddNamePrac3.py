4e1f432c0c60e3acf71e37a145c1161311f8ca0a
Juliusjanco <julius.arnoldjanco@my.jcu.edu.au>
2017-03-17 10:50:52 +1000

    adding oddname functionality

---
+++ oddNamePrac3.py
@@ -0,0 +1,30 @@
+def main():
+
+    get_name()
+
+
+def get_name():
+    userinput = input(">>")
+    try:
+        if ' ' in userinput:
+            print('Please Enter Name:')
+
+        elif '  ' in userinput:
+            print("Only one space please")
+
+        elif userinput is '':
+            print("Something has to be put in porkchop")
+
+        else:
+            count = 2
+            for each in list(userinput):
+                if count % 2 != 0:
+                    print(each)
+                    count += 1
+                else:
+                    count += 1
+    except SyntaxError:
+        print("must enter something")
+
+
+main()
